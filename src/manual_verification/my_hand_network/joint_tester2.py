3167138585e75fb589832d2c17994cd745cfa13e
m-richards <45483497+m-richards@users.noreply.github.com>
2020-09-09 22:28:32 +1000

    Example testing, probably not worth it

--- src/manual_verification/my_hand_network/joint_tester2.py
+++ src/manual_verification/my_hand_network/joint_tester2.py
@@ -12,21 +12,23 @@ np.set_printoptions(edgeitems=10, linewidth=300)
 # np.core.arrayprint._line_width = 500
 
 # DATA
+# silly deterministic network
+distances = np.array(
+    [[0, 5, 0, 4],
+     [0, 0, 3, 0],
+     [0, 3, 0, 5],
+     [4, 0, 0, 0]])
+
 
 # distances = np.array(
-#     [[4, 3.5, 3],
-#      [3.5, 3, 2.5],
-#      [3, 2.5, 2],
-#      ])
-distances = np.array(
-    [[4, 3.5, 4.5, 3, 3, 0, 0, 0],
-     [3.5, 3, 4, 0, 2.5, 3, 3, 0],
-     [4.5, 4, 5, 0, 0, 0, 4, 3.5],
-     [3, 0, 0, 2, 2, 2.5, 0, 2],
-     [3, 2.5, 0, 2, 2, 2.5, 2.5, 0],
-     [0, 3, 0, 2.5, 2.5, 3, 3, 2.5],
-     [0, 3, 4, 0, 2.5, 3, 3, 2.5],
-     [0, 0, 3.5, 2, 0, 2.5, 2.5, 2]])
+#     [[4, 3.5, 4.5, 3, 3, 0, 0, 0],
+#      [3.5, 3, 4, 0, 2.5, 3, 3, 0],
+#      [4.5, 4, 5, 0, 0, 0, 4, 3.5],
+#      [3, 0, 0, 2, 2, 2.5, 0, 2],
+#      [3, 2.5, 0, 2, 2, 2.5, 2.5, 0],
+#      [0, 3, 0, 2.5, 2.5, 3, 3, 2.5],
+#      [0, 3, 4, 0, 2.5, 3, 3, 2.5],
+#      [0, 0, 3.5, 2, 0, 2.5, 2.5, 2]])
 
 incidence_mat = (distances > 0).astype(int)
 
@@ -34,18 +36,14 @@ incidence_mat = (distances > 0).astype(int)
 data_list = [distances]
 network_struct = ModelDataStruct(data_list, incidence_mat,
                                           data_array_names_debug=("distances"))
-beta_known = -2
+beta_known = -0.4
 beta_vec_generate = np.array([beta_known])
 model = RecursiveLogitModelPrediction(network_struct,
                                       initial_beta=beta_vec_generate, mu=1)
-# obs_indices = [i for i in range(8)]
-# obs = model.generate_observations(origin_indices=obs_indices,
-#                                   dest_indices=obs_indices,
-#                                   num_obs_per_pair=1, iter_cap=2000, rng_seed=1,
-#                                   )
-obs_indices = [1]
+
+obs_indices = [0]
 obs = model.generate_observations(origin_indices=obs_indices,
-                                  dest_indices=[7],
+                                  dest_indices=[1],
                                   num_obs_per_pair=20, iter_cap=2000, rng_seed=1,
                                   )
 
@@ -55,7 +53,7 @@ print("\nPath in terms of arcs:")
 for path in obs:
     string = "Orig: "
     f = "Empty Path, should not happen"
-    for arc_index in path:
+    for arc_index in path[1:]:
         string += f"-{arc_index + 1}- => "
     string += ": Dest"
 
@@ -79,15 +77,15 @@ print("len ", len(obs_ak))
 
 #
 # DATA
-distances = np.array(
-    [[4, 3.5, 4.5, 3, 3, 0, 0, 0],
-     [3.5, 3, 4, 0, 2.5, 3, 3, 0],
-     [4.5, 4, 5, 0, 0, 0, 4, 3.5],
-     [3, 0, 0, 2, 2, 2.5, 0, 2],
-     [3, 2.5, 0, 2, 2, 2.5, 2.5, 0],
-     [0, 3, 0, 2.5, 2.5, 3, 3, 2.5],
-     [0, 3, 4, 0, 2.5, 3, 3, 2.5],
-     [0, 0, 3.5, 2, 0, 2.5, 2.5, 2]])
+# distances = np.array(
+#     [[4, 3.5, 4.5, 3, 3, 0, 0, 0],
+#      [3.5, 3, 4, 0, 2.5, 3, 3, 0],
+#      [4.5, 4, 5, 0, 0, 0, 4, 3.5],
+#      [3, 0, 0, 2, 2, 2.5, 0, 2],
+#      [3, 2.5, 0, 2, 2, 2.5, 2.5, 0],
+#      [0, 3, 0, 2.5, 2.5, 3, 3, 2.5],
+#      [0, 3, 4, 0, 2.5, 3, 3, 2.5],
+#      [0, 0, 3.5, 2, 0, 2.5, 2.5, 2]])
 
 distances = dok_matrix(distances)
 
@@ -98,8 +96,7 @@ data_list = [distances]
 network_struct = ModelDataStruct(data_list, incidence_mat,
                                           data_array_names_debug=("distances",))
 
-beta = -5
-beta_vec = np.array([beta])  # 4.96 diverges
+
 # optimiser = op.LineSearchOptimiser(op.OptimHessianType.BFGS, max_iter=40)
 #
 # model = RecursiveLogitModelEstimation(network_struct, observations_record=obs_ak,
@@ -119,34 +116,39 @@ beta_vec = np.array([beta])  # 4.96 diverges
 # =======================================================
 print(120 * "=", 'redo with scipy')
 optimiser = op.ScipyOptimiser(method='bfgs')
-
+beta = -0.4
+beta_vec = np.array([beta])  # 4.96 diverges
 model = RecursiveLogitModelEstimation(network_struct, observations_record=obs_ak,
                                       initial_beta=beta_vec, mu=1,
                                       optimiser=optimiser)
 log_like_out, grad_out = model.get_log_likelihood()
 print("start beta", beta, "log likelihood:", log_like_out)
 
-beta_out = model.solve_for_optimal_beta(verbose=True)
-print("optimal solve complete")
-# print(model.optim_function_state.val_grad_function(beta))
-print("start beta", beta, "likelihood:", np.exp(-log_like_out))
-print("best beta", beta_out, "likelihood:", np.exp(-model.optim_function_state.value))
-print("best beta known", beta_known, "likelihood:", np.exp(-model.eval_log_like_at_new_beta(
-    beta_known)[0]))
-print("best beta incorrect", model.get_beta_vec())
+# beta_out = model.solve_for_optimal_beta(verbose=True)
+# print("optimal solve complete")
+# # print(model.optim_function_state.val_grad_function(beta))
+# print("start beta", beta, "likelihood:", np.exp(-log_like_out))
+# print("best beta", beta_out, "likelihood:", np.exp(-model.optim_function_state.value))
+# print("best beta known", beta_known, "likelihood:", np.exp(-model.eval_log_like_at_new_beta(
+#     beta_known)[0]))
+# print("best beta incorrect", model.get_beta_vec())
+#
+import seaborn as sns
+sns.set()
+import matplotlib.pyplot as plt
+beta = np.arange(-0.1, -1, -0.05)
+like = np.zeros(len(beta))
+log_like = np.zeros(len(beta))
 
-# import matplotlib.pyplot as plt
-# beta = np.arange(-0.1, -4, -0.1)
-# like = np.zeros(len(beta))
-#
-# for i in range(len(beta)):
-#     like[i] = np.exp(-model.eval_log_like_at_new_beta(beta[i])[0])
-# plt.scatter(beta, like, label=rf'\beta_{{true}} = {beta_known}')
-# plt.legend()
-#
-# print("actually generated with beta= ", beta_known, "like = ", np.exp(
-#     -model.eval_log_like_at_new_beta(beta_known)[0]))
-#
-# plt.show()
+for i in range(len(beta)):
+    like[i] = np.exp(-model.eval_log_like_at_new_beta(beta[i])[0])
+    log_like[i] = -model.eval_log_like_at_new_beta(beta[i])[0]
+plt.scatter(beta, log_like, label=rf'$\beta_{{true}} = {beta_known}$')
+plt.legend()
+
+print("actually generated with beta= ", beta_known, "like = ", np.exp(
+    -model.eval_log_like_at_new_beta(beta_known)[0]))
+
+plt.show()
 # model.update_beta_vec(np.array([-16]))
 # print(model.get_log_likelihood())
