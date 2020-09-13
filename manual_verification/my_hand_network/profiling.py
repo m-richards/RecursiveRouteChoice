97d1c40e8c17bb2cb9f7e21c598cbc646bdd8623
m-richards <45483497+m-richards@users.noreply.github.com>
2020-09-13 13:12:21 +1000

    test files

---
+++ manual_verification/my_hand_network/profiling.py
@@ -0,0 +1,78 @@
+import numpy as np
+from scipy.sparse import dok_matrix
+
+from recursive_route_choice import RecursiveLogitModelPrediction, ModelDataStruct, \
+    RecursiveLogitModelEstimation
+
+import optimisers as op
+
+np.set_printoptions(edgeitems=10, linewidth=300)
+# np.core.arrayprint._line_width = 500
+
+distances = np.array(
+    [[4, 3.5, 4.5, 3, 3, 0, 0, 0],
+     [3.5, 3, 4, 0, 2.5, 3, 3, 0],
+     [4.5, 4, 5, 0, 0, 0, 4, 3.5],
+     [3, 0, 0, 2, 2, 2.5, 0, 2],
+     [3, 2.5, 0, 2, 2, 2.5, 2.5, 0],
+     [0, 3, 0, 2.5, 2.5, 3, 3, 2.5],
+     [0, 3, 4, 0, 2.5, 3, 3, 2.5],
+     [0, 0, 3.5, 2, 0, 2.5, 2.5, 2]])
+
+incidence_mat = (distances > 0).astype(int)
+
+
+data_list = [distances]
+network_struct = ModelDataStruct(data_list, incidence_mat,
+                                          data_array_names_debug=("distances"))
+beta_known = -1
+beta_vec_generate = np.array([beta_known])
+model = RecursiveLogitModelPrediction(network_struct,
+                                      initial_beta=beta_vec_generate, mu=1)
+
+obs_indices = [0, 3, 5]
+dest_indices = [1, 2, 7]
+obs_per_pair = 8
+print(f"Generating {obs_per_pair * len(obs_indices) * len(dest_indices)} obs total")
+
+
+def get_data(beta, seed=None):
+    beta_vec_generate = np.array([beta])
+    model = RecursiveLogitModelPrediction(network_struct,
+                                          initial_beta=beta_vec_generate, mu=1)
+    obs = model.generate_observations(origin_indices=obs_indices,
+                                      dest_indices=dest_indices,
+                                      num_obs_per_pair=obs_per_pair, iter_cap=2000, rng_seed=seed,
+                                      )
+    return obs
+
+distances = dok_matrix(distances)
+
+incidence_mat = (distances > 0).astype(int)
+data_list = [distances]
+network_struct = ModelDataStruct(data_list, incidence_mat,
+                                          data_array_names_debug=("distances",))
+
+print(120 * "=", 'redo with scipy')
+optimiser = op.ScipyOptimiser(method='l-bfgs-b') # bfgs, l-bfgs-b
+beta = -0.4
+beta_vec = np.array([beta])  # 4.96 diverges
+
+import time
+a = time.time()
+for n, beta_gen in enumerate(np.arange(-0.7, -1.2, -0.1), start=1):
+    try:
+        obs = get_data(beta_gen, seed=1)
+    except ValueError as e:
+        print(f"beta = {beta_gen} failed, {e}")
+        continue
+    # print(obs)
+    beta_init = -1.0
+    model = RecursiveLogitModelEstimation(network_struct, observations_record=obs,
+                                          initial_beta=beta_init, mu=1,
+                                          optimiser=optimiser)
+    beta = model.solve_for_optimal_beta(verbose=False)
+    print("beta_expected", beta_gen, "beta actual", beta)
+
+b = time.time()
+print("elapsed =", b-a, "s")
