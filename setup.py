cece38b7808df85632fb0fa7f395a87800541511
m-richards <45483497+m-richards@users.noreply.github.com>
2020-11-19 16:51:19 +1000

    Fix setup.py merge

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup, find_packages
 import os
+
 setup(name="recursiveRouteChoice",
       package_dir={'': os.path.join('src', 'recursiveRouteChoice')},
-      packages=find_packages(where=os.path.join('src', 'recursiveRouteChoice')))
+      packages=find_packages(where=os.path.join('src', 'recursiveRouteChoice')),
       version="0.1",
       description="Recursive logit model implementation in python",
       author="Matthew Richards",
       url="https://github.com/m-richards/RecursiveRouteChoice",
       classifiers=[
-            "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3",
       ],
       python_requires='>=3.6',
       install_requires=[
-            "numpy",
-            "scipy",
-            "awkward1",
-            "pandas"
+          "numpy",
+          "scipy",
+          "awkward1",
+          "pandas"
       ]
       )
